8b21384a7e39f7d602a9dfc125cb02d62bb386fa
blake-dreyer <blakedreyerdev@gmail.com>
2022-08-04 14:44:28 -0400

    Added Schemas and DB, started to rework main

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -1,7 +1,41 @@
-from fastapi import FastAPI
+import secrets
+from urllib import response
+ 
+import validators
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+
+from . import models, schemas
+from .database import SessionLocal, engine
 
 app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal
+    try:
+        yield db
+    finally:
+        db.close()
+
+def raise_bad_request(message):
+    raise HTTPException(status_code=400, detail=message)
+
 
 @app.get("/")
 def read_root():
     return "Welcome to the URL shortener API :)"
+
+@app.post("/url", response_model=schemas.URLInfo)
+def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
+    if not validators.url(url.target_url):
+        raise_bad_request(message="Your provided URL is not valid")
+
+    chars = "ABCDEFGHIJLKMONOPQRSTUVWXYZ"
+    #STOPPED HERE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  
+   
+   
+   
+   
+   
+    return f"TODO: Create database entry for: {url.target_url}"
